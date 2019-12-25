b051c5300e668112b80128bab4e48e184260acff
Yury Tsarev <xnull@opensuse.org>
2019-12-25 23:55:01 +0100

    Mock out sleep call and build count based assertion
    Use DI and interface for that

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -4,12 +4,32 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+const finalWord = "Go!"
+const countdownStarts = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStarts; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
 }
