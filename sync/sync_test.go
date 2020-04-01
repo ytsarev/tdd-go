05f846521f4baa16d4852020ec08580310dd3c74
Yury Tsarev <yury.tsarev@absa.africa>
2020-04-02 00:23:00 +0200

    WaitGroup based tests and Mutex to solve concurrency

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,6 +1,9 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
@@ -11,6 +14,25 @@ func TestCounter(t *testing.T) {
 
 		assertCounter(t, counter, 3)
 	})
+
+	t.Run("it runs concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Inc()
+				w.Done()
+			}(&wg)
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
+
 }
 
 func assertCounter(t *testing.T, got Counter, want int) {
