6697e4fa07499ebf4d73962820d3096f1d5db38d
Yury Tsarev <yury.tsarev@absa.africa>
2020-04-25 23:53:17 +0200

    Table test foundation

--- property/numeral_test.go
+++ property/numeral_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "I"
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+		{"3 gets converted to II", 3, "III"},
+	}
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("got %q, want %q", got, test.Want)
+			}
+		})
 	}
 }
