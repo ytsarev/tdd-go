ea28fa0aaaad8151849edab23910649b371580f5
Yury Tsarev <yury.tsarev@absa.africa>
2020-06-18 11:23:15 +0200

    Switch to uint16

--- property/numeral_test.go
+++ property/numeral_test.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"testing"
 	"testing/quick"
 )
 
 var cases = []struct {
-	Arabic int
+	Arabic uint16
 	Roman  string
 }{
 	{Arabic: 1, Roman: "I"},
@@ -65,11 +64,11 @@ func TestConvertingToArabic(t *testing.T) {
 }
 
 func TestPropertiesOfConversion(t *testing.T) {
-	assertion := func(arabic int) bool {
-		if arabic < 0 || arabic > 3999 {
-			log.Println(arabic)
+	assertion := func(arabic uint16) bool {
+		if arabic > 3999 {
 			return true
 		}
+		t.Log("testing", arabic)
 		roman := ConvertToRoman(arabic)
 		fromRoman := ConvertToArabic(roman)
 		return fromRoman == arabic
